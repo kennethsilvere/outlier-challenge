f231c7baeebac08652e7283ec26e977d95f1e1d4
Kenneth Silvere <kennethsilvere93@gmail.com>
2022-10-07 08:20:41 +0530

    Fix tests and format code using StandardJS

--- api.js
+++ api.js
@@ -3,115 +3,114 @@ const request = require('request')
 const JSONStream = require('JSONStream')
 
 module.exports = {
-	getHealth,
-	getStudent,
-	getStudentGradesReport,
-	getCourseGradesReport
+  getHealth,
+  getStudent,
+  getStudentGradesReport,
+  getCourseGradesReport
 }
 
-async function getHealth(req, res, next) {
-	try {
-		await knex('students').first()
-		return res.status(200).json({ success: true })
-	} catch (e) {
-		console.log(e)
-		return res.status(500).end()
-	}
+async function getHealth (req, res, next) {
+  try {
+    await knex('students').first()
+    return res.status(200).json({ success: true })
+  } catch (e) {
+    console.log(e)
+    return res.status(500).end()
+  }
 }
 
-async function getStudent(req, res, next) {
-	try {
-		const studentData = await getStudentDataById(req.params.id)
-		return res.status(200).json(studentData)
-	} catch (e) {
-		console.log(e)
-		return res.status(500).end()
-	}
+async function getStudent (req, res, next) {
+  try {
+    const studentData = await getStudentDataById(req.params.id)
+    return res.status(200).json(studentData)
+  } catch (e) {
+    console.log(e)
+    return res.status(500).end()
+  }
 }
 
-async function getStudentGradesReport(req, res, next) {
-	try {
-		const id = req.params.id
-		const studentData = await getStudentDataById(id)
-		return mergeWithCourseGradesFromRemoteSource(id, studentData, res)
-	} catch (e) {
-		console.log(e)
-		return res.status(500).end()
-	}
+async function getStudentGradesReport (req, res, next) {
+  try {
+    const id = req.params.id
+    const studentData = await getStudentDataById(id)
+    return mergeWithCourseGradesFromRemoteSource(id, studentData, res)
+  } catch (e) {
+    console.log(e)
+    return res.status(500).end()
+  }
 }
 
-async function getCourseGradesReport(req, res, next) {
-	try {
-		return getCourseStatistics(res)
-	} catch (e) {
-		console.log(e)
-		return res.status(500).end()
-	}
+async function getCourseGradesReport (req, res, next) {
+  try {
+    return getCourseStatistics(res)
+  } catch (e) {
+    console.log(e)
+    return res.status(500).end()
+  }
 }
 
-async function getStudentDataById(id) {
-	const studentData = await knex('students').select().where('id', id)
-	return studentData[0]
+async function getStudentDataById (id) {
+  const studentData = await knex('students').select().where('id', id)
+  return studentData[0]
 }
 
-async function mergeWithCourseGradesFromRemoteSource(
-	idFromDB,
-	studentData,
-	res
+async function mergeWithCourseGradesFromRemoteSource (
+  idFromDB,
+  studentData,
+  res
 ) {
-	let courseGrades = {}
-	request('https://outlier-coding-test-data.netlify.app/grades.json')
-		.pipe(JSONStream.parse('*'))
-		.on('data', (gradesData) => {
-			idFromDB = +idFromDB
-			const idFromJSON = +gradesData.id
-			if (idFromJSON === idFromDB) {
-				courseGrades[gradesData.course] = gradesData.grade
-			}
-		})
-		.on('end', () => {
-			const studentDataWithGrades = {
-				...studentData,
-				course_grades: courseGrades
-			}
-			return res.status(200).json(studentDataWithGrades)
-		})
+  const courseGrades = {}
+  request('https://outlier-coding-test-data.netlify.app/grades.json')
+    .pipe(JSONStream.parse('*'))
+    .on('data', (gradesData) => {
+      idFromDB = +idFromDB
+      const idFromJSON = +gradesData.id
+      if (idFromJSON === idFromDB) {
+        courseGrades[gradesData.course] = gradesData.grade
+      }
+    })
+    .on('end', () => {
+      const studentDataWithGrades = {
+        ...studentData,
+        course_grades: courseGrades
+      }
+      return res.status(200).json(studentDataWithGrades)
+    })
 }
 
-async function getCourseStatistics(res) {
-	const courses = {}
+async function getCourseStatistics (res) {
+  const courses = {}
 
-	request('https://outlier-coding-test-data.netlify.app/grades.json')
-		.pipe(JSONStream.parse('*'))
-		.on('data', (d) => {
-			const courseName = d.course
-			if (!courses[courseName]) {
-				courses[courseName] = {
-					highestGrade: +d.grade,
-					lowestGrade: +d.grade,
-					averageGrade: {
-						count: 0,
-						...d
-					}
-				}
-			}
+  request('https://outlier-coding-test-data.netlify.app/grades.json')
+    .pipe(JSONStream.parse('*'))
+    .on('data', (d) => {
+      const courseName = d.course
+      if (!courses[courseName]) {
+        courses[courseName] = {
+          highestGrade: +d.grade,
+          lowestGrade: +d.grade,
+          averageGrade: {
+            count: 0,
+            ...d
+          }
+        }
+      }
 
-			if (courses[courseName].highestGrade < +d.grade) {
-				courses[courseName].highestGrade = +d.grade
-			}
-			if (courses[courseName].lowestGrade > +d.grade) {
-				courses[courseName].lowestGrade = +d.grade
-			}
-			courses[courseName].averageGrade.grade += +d.grade
-			courses[courseName].averageGrade.count++
-		})
-		.on('end', () => {
-			for (const key of Object.keys(courses)) {
-				courses[key].averageGrade = +(
-					courses[key].averageGrade.grade /
-					courses[key].averageGrade.count
-				).toFixed(2)
-			}
-			return res.status(200).json(courses)
-		})
+      if (courses[courseName].highestGrade < +d.grade) {
+        courses[courseName].highestGrade = +d.grade
+      }
+      if (courses[courseName].lowestGrade > +d.grade) {
+        courses[courseName].lowestGrade = +d.grade
+      }
+      courses[courseName].averageGrade.grade += +d.grade
+      courses[courseName].averageGrade.count++
+    })
+    .on('end', () => {
+      for (const key of Object.keys(courses)) {
+        courses[key].averageGrade = +(
+          courses[key].averageGrade.grade / courses[key].averageGrade.count
+        ).toFixed(2)
+      }
+      return res.status(200).json(courses)
+    })
 }
--- test.js
+++ test.js
@@ -8,99 +8,99 @@ const endpoint = `http://localhost:${port}`
 const server = require('./server')
 
 tape('health', async function (t) {
-	const url = `${endpoint}/health`
-	try {
-		const { data, response } = await jsonist.get(url)
-		if (response.statusCode !== 200) {
-			throw new Error(
-				'Error connecting to sqlite database; did you initialize it by running `npm run init-db`?'
-			)
-		}
-		t.ok(data.success, 'should have successful healthcheck')
-		t.end()
-	} catch (e) {
-		t.error(e)
-	}
+  const url = `${endpoint}/health`
+  try {
+    const { data, response } = await jsonist.get(url)
+    if (response.statusCode !== 200) {
+      throw new Error(
+        'Error connecting to sqlite database; did you initialize it by running `npm run init-db`?'
+      )
+    }
+    t.ok(data.success, 'should have successful healthcheck')
+    t.end()
+  } catch (e) {
+    t.error(e)
+  }
 })
 
 tape('getStudent', async function (t) {
-	const url = `${endpoint}/student/43`
-	try {
-		const { data, response } = await jsonist.get(url)
-		if (response.statusCode !== 200) {
-			throw new Error(
-				'Error connecting to sqlite database; did you initialize it by running `npm run init-db`?'
-			)
-		}
-		t.ok(
-			testValidations.validateAgainstStudentData(data),
-			'should have successful healthcheck'
-		)
-		t.end()
-	} catch (e) {
-		t.error(e)
-	}
+  const url = `${endpoint}/student/43`
+  try {
+    const { data, response } = await jsonist.get(url)
+    if (response.statusCode !== 200) {
+      throw new Error(
+        'Error connecting to sqlite database; did you initialize it by running `npm run init-db`?'
+      )
+    }
+    t.ok(
+      testValidations.validateAgainstStudentData(data),
+      'should have successful healthcheck'
+    )
+    t.end()
+  } catch (e) {
+    t.error(e)
+  }
 })
 
 tape('getStudent', async function (t) {
-	const url = `${endpoint}/student/43`
-	try {
-		const { data, response } = await jsonist.get(url)
-		if (response.statusCode !== 200) {
-			throw new Error(
-				'Error connecting to sqlite database; did you initialize it by running `npm run init-db`?'
-			)
-		}
-		t.ok(
-			testValidations.validateAgainstStudentData(data),
-			'should return the student data for the student of id 43'
-		)
-		t.end()
-	} catch (e) {
-		t.error(e)
-	}
+  const url = `${endpoint}/student/43`
+  try {
+    const { data, response } = await jsonist.get(url)
+    if (response.statusCode !== 200) {
+      throw new Error(
+        'Error connecting to sqlite database; did you initialize it by running `npm run init-db`?'
+      )
+    }
+    t.ok(
+      testValidations.validateAgainstStudentData(data),
+      'should return the student data for the student of id 43'
+    )
+    t.end()
+  } catch (e) {
+    t.error(e)
+  }
 })
 
 tape('getStudentGradesReport', async function (t) {
-	const url = `${endpoint}/student/82`
-	try {
-		const { data, response } = await jsonist.get(url)
-		if (response.statusCode !== 200) {
-			throw new Error(
-				'Error connecting to sqlite database; did you initialize it by running `npm run init-db`?'
-			)
-		}
-		t.ok(
-			testValidations.validateAgainstStudentGrades(data),
-			'should return the student grades report for the student of id 82'
-		)
-		t.end()
-	} catch (e) {
-		t.error(e)
-	}
+  const url = `${endpoint}/student/82`
+  try {
+    const { data, response } = await jsonist.get(url)
+    if (response.statusCode !== 200) {
+      throw new Error(
+        'Error connecting to sqlite database; did you initialize it by running `npm run init-db`?'
+      )
+    }
+    t.ok(
+      testValidations.validateAgainstStudentGrades(data),
+      'should return the student grades report for the student of id 82'
+    )
+    t.end()
+  } catch (e) {
+    t.error(e)
+  }
 })
 
 tape('getCourseGradesReport', async function (t) {
-	const url = `${endpoint}/course/all/grades`
-	try {
-		const { data, response } = await jsonist.get(url)
-		if (response.statusCode !== 200) {
-			throw new Error(
-				'Error connecting to sqlite database; did you initialize it by running `npm run init-db`?'
-			)
-		}
-		t.ok(
-			testValidations.validateAgainstCourseStatistics(data),
-			'should return the course grades statistics for all the students'
-		)
-		t.end()
-	} catch (e) {
-		t.error(e)
-	}
+  const url = `${endpoint}/course/all/grades`
+  try {
+    const { data, response } = await jsonist.get(url)
+    if (response.statusCode !== 200) {
+      throw new Error(
+        'Error connecting to sqlite database; did you initialize it by running `npm run init-db`?'
+      )
+    }
+    t.ok(
+      testValidations.validateAgainstCourseStatistics(data),
+      'should return the course grades statistics for all the students'
+    )
+    t.end()
+  } catch (e) {
+    t.error(e)
+  }
 })
 
 tape('cleanup', function (t) {
-	server.closeDB()
-	server.close()
-	t.end()
+  server.closeDB()
+  server.close()
+  t.end()
 })
